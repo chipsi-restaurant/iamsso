da352742cf81f3dc3e44c00d6bdf09408e865d88
sinusxd <senya.kolyshkin.gg@gmail.com>
2026-04-07 22:54:08 +0300

    infra: add Redis to docker-compose and iamsso_notifications database

--- infra/postgres/init-databases.sh
+++ infra/postgres/init-databases.sh
@@ -5,4 +5,5 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE DATABASE iamsso_auth;
     CREATE DATABASE iamsso_users;
     CREATE DATABASE iamsso_policies;
+    CREATE DATABASE iamsso_notifications;
 EOSQL
